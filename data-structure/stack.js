1b810bf2542fa7939afea20ec90b8f9851ca9869
Blenda C <50850913+blendacosta@users.noreply.github.com>
2021-09-21 20:24:13 -0300

    Edit Stack class

--- data-structure/stack.js
+++ data-structure/stack.js
@@ -1,61 +1,43 @@
 /**
- *  Stacks (Pilhas) => LIFO;
- *  
- *  Interface:
- *  1. Constructor Function
- *      1.1. Storage
- *  2. Methods
- *      2.1. Push(el) => adds an element to the stack
- *      2.2. Pop() => removes and returns the last element of the stack
- *      2.3. Peek() => returns the last element of the stack
- *      2.4. Size() => returns an integer with the size of the stack
- *      2.5. isEmpty()
- *      2.6. isFull()
+ *  Stacks (Pilhas) >> LIFO;
  */
 
-
 class Stack {
-    //interface
-    constructor(capacity) {
-        this._capacity = capacity || Infinity;
-        this._items = {};
-        this._count = 0;
-    }
-    //methods
-    push(el) {
-        if (this._count < this._capacity) {
-            this._items[this._count] = el;
-            this._count++;
-        } else {
-            console.log('Stack is full!');
-        }
-        return this;
-    }
-
-    pop() {
-        const value = this._items[this._count - 1]; // get the value of last item
-        delete this._items[this._count - 1]; // delete this item
-        this._count--; // updates the counter
-
-        if (this._count < 0) {
-            this._count = 0;
-        }
-        return value;
-    }
-
-    peek() {
-        return this._items[this._count - 1];
+    constructor() {
+        this.items = [];
+        this.top = null;
     }
 
     size() {
-        return this._count;
+        return this.items.length;
     }
 
     isEmpty() {
-        return this._count === 0;
+        return this.size() === 0;
     }
 
-    isFull() {
-        return this._count === this._capacity;
+    getTop() {
+        if (this.isEmpty())
+            return null;
+        return this.top;
+    }
+
+    push(element) {
+        this.items.push(element);
+        this.top = element;
+    }
+
+    pop() {
+        if (!this.isEmpty()) {
+            if (this.size() === 1) {
+                this.top = null;
+                return this.items.pop();
+            } else {
+                this.top = this.items[this.size() - 2];
+                return this.items.pop();
+            }
+        } else {
+            return null;
+        }
     }
 }
