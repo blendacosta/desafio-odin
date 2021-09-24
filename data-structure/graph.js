1235eb2b08d5c2186ac6abb59a75e8b1d8f2afcf
Blenda C <50850913+blendacosta@users.noreply.github.com>
2021-09-24 19:51:33 -0300

    Add BFS and DFS in graph

--- data-structure/graph.js
+++ data-structure/graph.js
@@ -1,68 +1,122 @@
+const Queue = require('./queue.js');
+
 class Graph {
-    constructor(numberOfVertices) {
-      this.numberOfVertices = numberOfVertices
-      this.adjacencyList = new Map();
-    }
-  
-    addVertex(value) {
-      if (!this.adjacencyList.has(value)) {
-        this.adjacencyList.set(value, new Set([]))
-      }
-    }
-  
-    removeVertex(value) {
-      this.adjacencyList.forEach((list) => {
-        if (list.has(value)) {
-          list.delete(value)
-        }
-      })
-  
-      this.adjacencyList.delete(value)
-    }
-  
-    addEdge(source, destination) {
-      this.adjacencyList.get(source).add(destination)
-      this.adjacencyList.get(destination).add(source)
-    }
-  
-    removeEdge(source, destination) {
-      this.adjacencyList.get(source).delete(destination)
-      this.adjacencyList.get(destination).delete(source)
-    }
-  
-    print() {
-      const vertices = this.adjacencyList.keys()
-  
-      for (let i of vertices) {
-        const values = this.adjacencyList.get(i);
-  
-        let string = "";
-  
-        for (let j of values) {
-          string += `${j} `
-        }
-  
-        console.log(`${i} -> ${string}`)
-      }
+  constructor(direction = 'undirected') {
+    this.direction = direction
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value) {
+    if (!this.adjacencyList.has(value)) {
+      this.adjacencyList.set(value, new Set([]))
     }
   }
-  
-  module.exports = Graph
-  
-  const graph = new Graph()
-  
-  graph.addVertex('a')
-  graph.addVertex('b')
-  graph.addVertex('c')
-  graph.addVertex('d')
-  
-  graph.addEdge('a', 'a')
-  graph.addEdge('a', 'b')
-  graph.addEdge('b', 'c')
-  graph.addEdge('c', 'd')
-  graph.addEdge('d', 'b')
-  graph.addEdge('d', 'c')
-  
-  graph.removeEdge('a', 'b')
-  
-  graph.print()
+
+  removeVertex(value) {
+    this.adjacencyList.forEach((list) => {
+      if (list.has(value)) {
+        list.delete(value)
+      }
+    })
+
+    this.adjacencyList.delete(value)
+  }
+
+  addEdge(source, destination) {
+    this.adjacencyList.get(source).add(destination)
+
+    if (this.direction === 'undirected') {
+      this.adjacencyList.get(destination).add(source)
+    }
+  }
+
+  removeEdge(source, destination) {
+    this.adjacencyList.get(source).delete(destination)
+
+    if (this.direction === 'undirected') {
+      this.adjacencyList.get(destination).delete(source)
+    }
+  }
+
+  breadthFirstSearch(startingNode) {
+    let visited = new Map();
+    let q = new Queue();
+
+    visited.set(startingNode, true)
+
+    q.enqueue(startingNode)
+
+    while(!q.isEmpty()) {
+      const element = q.dequeue()
+
+      const adjacencyListForCurrentVertex = this.adjacencyList.get(element)
+
+      adjacencyListForCurrentVertex.forEach((neighbour) => {
+        if (!visited.get(neighbour)) {
+          visited.set(neighbour, true)
+          q.enqueue(neighbour)
+        }
+      })
+    }
+
+    console.log(visited.keys())
+
+    return visited.keys();
+  }
+
+  depthFirstSearch(startingNode) {
+    let visited = new Map()
+
+    this.processAdjacentVertices(startingNode, visited)
+
+    console.log(visited.keys())
+
+    return visited.keys();
+  }
+
+  processAdjacentVertices(startingNode, visited) {
+    visited.set(startingNode, true)
+
+    const neighbours = this.adjacencyList.get(startingNode)
+
+    neighbours.forEach((neighbour) => {
+      if (!visited.get(neighbour)) {
+        this.processAdjacentVertices(neighbour, visited)
+      }
+    })
+  }
+
+  print() {
+    const vertices = this.adjacencyList.keys()
+
+    for (let i of vertices) {
+      const values = this.adjacencyList.get(i);
+
+      let string = "";
+
+      for (let j of values) {
+        string += `${j} `
+      }
+
+      console.log(`${i} -> ${string}`)
+    }
+  }
+}
+
+module.exports = Graph
+//
+// const graph = new Graph()
+// const vertices = ['a', 'b', 'c', 'd', 'e', 'f']
+//
+// vertices.forEach((v) => graph.addVertex(v))
+//
+// graph.addEdge('a', 'b')
+// graph.addEdge('a', 'c')
+// graph.addEdge('a', 'd')
+// graph.addEdge('b', 'e')
+// graph.addEdge('c', 'f')
+//
+// graph.print()
+
+// graph.breadthFirstSearch('a')
+// graph.depthFirstSearch('a')
